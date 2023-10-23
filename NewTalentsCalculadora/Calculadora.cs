2d3d32d4854bfa0e56afef5f811314263d787c97
Lucas Nabesima <lucas.nabesima@gmail.com>
2023-10-23 16:23:49 -0300

    feat: implement basic calculator functionality

--- NewTalentsCalculadora/Calculadora.cs
+++ NewTalentsCalculadora/Calculadora.cs
@@ -4,22 +4,23 @@ public class Calculadora
 {
     public int Somar(int num1, int num2)
     {
-        return 0;
+        return num1 + num2;
     }
 
     public int Subtrair(int num1, int num2)
     {
-        return 0;
+        return num1 - num2;
     }
 
     public int Multiplicar(int num1, int num2)
     {
-        return 0;
+        return num1 * num2;
     }
 
     public int Dividir(int num1, int num2)
     {
-        return 0;
+        if (num2 == 0) throw new DivideByZeroException();
+        return num1 / num2;
     }
 
     public List<string> Historico()
