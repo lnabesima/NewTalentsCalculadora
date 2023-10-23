a6252d71c98b09b5fc7bbc9021929751082c8141
Lucas Nabesima <lucas.nabesima@gmail.com>
2023-10-23 12:32:03 -0300

    feat: create basic structures for methods in Calculadora class

---
+++ NewTalentsCalculadora/Calculadora.cs
@@ -0,0 +1,29 @@
+﻿namespace NewTalentsCalculadora;
+
+public class Calculadora
+{
+    public int somar(int num1, int num2)
+    {
+        return 0;
+    }
+
+    public int subtrair(int num1, int num2)
+    {
+        return 0;
+    }
+
+    public int multiplicar(int num1, int num2)
+    {
+        return 0;
+    }
+
+    public int dividir(int num1, int num2)
+    {
+        return 0;
+    }
+
+    public List<string> historico()
+    {
+        return new List<string>();
+    }
+}
