7bfc7be953477745b7e633daedd5897a689d964d
Lucas Nabesima <lucas.nabesima@gmail.com>
2023-10-23 16:44:57 -0300

    feat: implement the Historico method

--- NewTalentsCalculadora/Calculadora.cs
+++ NewTalentsCalculadora/Calculadora.cs
@@ -2,29 +2,49 @@
 
 public class Calculadora
 {
+    private List<string> _listaHistorico;
+
+    public Calculadora()
+    {
+        _listaHistorico = new List<string>();
+    }
+
     public int Somar(int num1, int num2)
     {
-        return num1 + num2;
+        var res = num1 + num2;
+        _listaHistorico.Insert(0, $"{num1} + {num2} = {res}");
+
+        return res;
     }
 
     public int Subtrair(int num1, int num2)
     {
-        return num1 - num2;
+        var res = num1 - num2;
+        _listaHistorico.Insert(0, $"{num1} - {num2} = {res}");
+
+        return res;
     }
 
     public int Multiplicar(int num1, int num2)
     {
-        return num1 * num2;
+        var res = num1 * num2;
+        _listaHistorico.Insert(0, $"{num1} * {num2} = {res}");
+
+        return res;
     }
 
     public int Dividir(int num1, int num2)
     {
         if (num2 == 0) throw new DivideByZeroException();
-        return num1 / num2;
+        var res = num1 / num2;
+        _listaHistorico.Insert(0, $"{num1} / {num2} = {res}");
+
+        return res;
     }
 
     public List<string> Historico()
     {
-        return new List<string>();
+        _listaHistorico.RemoveRange(3, _listaHistorico.Count - 3);
+        return _listaHistorico;
     }
 }
